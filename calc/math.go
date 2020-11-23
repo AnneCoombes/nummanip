4c4bb7485afd419df6feb9d16d41871ecff7f09a
Anne Coombes <anne@besabeau.com>
2020-11-23 12:31:03 +0000

    new release version 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,10 +1,13 @@
 package calc
 
-func Add(numbers ...int) int {
+func Add(numbers ...int) (error, int) {
 	sum := 0
 
+	if len(numbers) < 2 {
+		return errors.new("provide more than 2 numbers"), sum
+	}
 	for _, num := range numbers {
 		sum = sum + num
 	}
-	return sum
+	return nil, sum
 }
